263a42689fd0f4d537beb112b556c888a1a07507
bsGestahl <b.spiegl89@gmx.at>
2023-05-02 17:25:44 +0200

    README.md update: added note to retrieve EGA dataset and 2bit reference genome file first changed profiling script call flag to correct one

--- driver_scripts/drv_compute_GC_presets.sh
+++ driver_scripts/drv_compute_GC_presets.sh
@@ -89,9 +89,9 @@ do
     if [ ! -d "${preset_out_dir}" ]; then
       mkdir -p "${preset_out_dir}"
     fi
-    "${python3_path}" "${profiling_script}" --include-children --iter 2 --sampling-frequency 10 \
+    "${python3_path}" "${profiling_script}" --track-spawns --iter 2 --sampling-frequency 10 \
     --output-path "${test_output_dir}" --script "${GCparagon_script}" --use-parameter-preset "${preset}" \
     --bam "${test_bam}" --two-bit-reference-genome "${TWOBIT_REF_GENOME}" --out-dir "${preset_out_dir}" \
-    --temporary-directory "${tmp_out_dir}" --write-chunk-exclusion --threads "${n_processes}" --track-spawns
+    --temporary-directory "${tmp_out_dir}" --write-chunk-exclusion --threads "${n_processes}"
   done
 done

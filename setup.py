42790abfc70ad8a57c5cf0bd753917463e6e05cf
isy89 <lazzeri89@gmail.com>
2024-04-11 11:22:13 +0200

    - include definition file for singularity image: singularity_definition_file/gcparagon.def - include CHANGELOG.md - bumping the version in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='GCparagon',
-    version='0.5.4',
+    version='0.5.5',
     description='commandline tool to correct GC bias in cfDNA WGS data',
     author='Benjamin Spiegl',
     author_email='benjamin.spiegl@medunigraz.at',
